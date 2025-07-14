04d305f60eab28123457b516ce0565b50f60c7d9
change <chang0306@foxmail.com>
2025-07-14 18:53:33 +0800

    update

--- Frontend(React)/Service/utils/md5.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-function md5(str) {
-  const hash = crypto.createHash("md5");
-  hash.update(str);
-  return hash.digest("hex"); //16进制
-}
-
-module.exports = md5;
