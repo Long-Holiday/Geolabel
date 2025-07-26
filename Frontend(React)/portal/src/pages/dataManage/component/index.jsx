2e7f85968e7ca7296cb15529bf24ec7736f0fcc8
Jiaqi Chang <125721536+Long-Holiday@users.noreply.github.com>
2025-07-26 12:01:40 +0800

    Delete Frontend(React)/portal directory

--- Frontend(React)/portal/src/pages/dataManage/component/index.jsx
+++
@@ -1,8 +0,0 @@
-import { Modal } from 'antd';
-export default ({ open, onCancel, title, uploadItem }) => {
-  return (
-    <Modal open={open} title={title} onCancel={onCancel} footer={null} destroyOnClose={true}>
-      {uploadItem}
-    </Modal>
-  );
-};
--- Frontend(React)/portal/src/services/register/api.js
+++
@@ -1,7 +0,0 @@
-import { request } from 'umi';
-export async function register(data) {
-  return request('/wegismarkapi/user/register', {
-    method: 'POST',
-    data,
-  });
-}
